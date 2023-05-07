c4b8f146de95d5c47090c7e04940d02c76c8bb31
cjje <gj2353@columbia.edu>
2023-05-07 17:29:56 -0400

    processing google img, training set up etc.

--- data.py
+++ data.py
@@ -4,15 +4,17 @@ import pickle as pkl
 import torch
 
 
-data_dir = './gans_data/data_dg'
+#data_dir = './gans_data/data_dg'
 
 
 # dataset for GANs
 class GANs_Dataset(torch.utils.data.Dataset):
-    def __init__(self):
+    def __init__(self, data_dir= './gans_data/data_dg', max_cls_num=5):
         """
         load training samples for GANs
         """
+        self.max_cls_num = max_cls_num
+        self.data_dir = data_dir
         
         with open(f'{data_dir}/image_list', 'rb') as f:
             self.image_list = pkl.load(f)
@@ -27,8 +29,8 @@ class GANs_Dataset(torch.utils.data.Dataset):
         cls = self.classes_list[index]
         ann = self.annotations_list[index]
         
-        cls = torch.cat([cls] + [torch.tensor([-1]) for _ in range(5 - cls.shape[0])], dim=0)
-        ann = torch.cat([ann] + [torch.tensor([-1, -1, -1, -1]).unsqueeze(0) for _ in range(5 - ann.shape[0])], dim=0)
+        cls = torch.cat([cls] + [torch.tensor([-1]) for _ in range(self.max_cls_num - cls.shape[0])], dim=0)
+        ann = torch.cat([ann] + [torch.tensor([-1, -1, -1, -1]).unsqueeze(0) for _ in range(self.max_cls_num - ann.shape[0])], dim=0)
             
         return (self.image_list[index], cls, ann)
     
